12bf6ff2c9a22fa5b40dad189a12c77b58125b96
Andoni <62946099+andiricum2@users.noreply.github.com>
2025-11-11 13:03:05 +0100

    Arreglar Chat controller

--- src/modules/chat/chat.controller.js
+++ src/modules/chat/chat.controller.js
@@ -36,35 +36,3 @@ export default {
   updateMessage,
   deleteMessage
 }
-import { created, noContent, ok } from '../../utils/ApiResponse.js'
-import { asyncHandler } from '../../middlewares/asyncHandler.js'
-import { UserService } from './user.service.js'
-
-const userService = new UserService()
-
-export const listUsers = asyncHandler(async (req, res) => {
-  const data = await userService.listUsers()
-  ok(res, data)
-})
-
-export const getUser = asyncHandler(async (req, res) => {
-  const data = await userService.getUser(req.params.id)
-  ok(res, data)
-})
-
-export const createUser = asyncHandler(async (req, res) => {
-  const data = await userService.createUser(req.body)
-  created(res, data)
-})
-
-export const updateUser = asyncHandler(async (req, res) => {
-  const data = await userService.updateUser(req.params.id, req.body)
-  ok(res, data)
-})
-
-export const deleteUser = asyncHandler(async (req, res) => {
-  await userService.deleteUser(req.params.id)
-  noContent(res)
-})
-
-
