35eda6487787caf5f876d8461ef082ec2411b314
Andoni <62946099+andiricum2@users.noreply.github.com>
2025-11-17 08:41:06 +0100

    Update index.js

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,6 +3,8 @@ import { Router } from 'express'
 import usersRoutes from './users.routes.js'
 import chatRoutes from './chat.routes.js'
 import postsRoutes from './posts.routes.js'
+// import bugsRoutes from './bugs.routes.js'
+// import statusRoutes from './status.routes.js'
 
 //router
 const router = Router()
@@ -11,8 +13,8 @@ const router = Router()
 router.use('/users', usersRoutes)
 router.use('/chat', chatRoutes)
 router.use('/posts', postsRoutes)
-router.use('/bugs', bugsRoutes)
-router.use('/status', statusRoutes)
+// router.use('/bugs', bugsRoutes)
+// router.use('/status', statusRoutes)
 
 //EXPORT router
 export default router
