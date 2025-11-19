fd269d792571cc2231cc6704ca02b569b3c2638e
gontxal00 <gontxal00@gmail.com>
2025-11-19 10:31:53 +0100

    historial de pings

--- src/routes/v1/status.routes.js
+++ src/routes/v1/status.routes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import Joi from 'joi'
-import { getStatus, pingStatus } from '../../modules/status/status.index.js'
+import { getStatus, pingStatus, getPingHistory } from '../../modules/status/status.index.js'
 
 const router = Router()
 
@@ -8,19 +8,24 @@ function validate(schema) {
   return (req, res, next) => {
     const toValidate = ["GET", "DELETE"].includes(req.method) ? req.params : req.body
     const result = schema.validate(toValidate, { abortEarly: false, stripUnknown: true })
+
     if (result.error) {
       const details = result.error.details.map(d => d.message)
-      return res.status(400).json({ success: false, error: { code: "VALIDATION_ERROR", message: "Datos inválidos", details } })
+      return res.status(400).json({
+        success: false,
+        error: { code: "VALIDATION_ERROR", message: "Datos inválidos", details }
+      })
     }
+
     if (["POST", "PUT", "PATCH"].includes(req.method)) req.body = result.value
     else req.params = result.value
+
     next()
   }
 }
 
-// RUTAS STATUS
 router.get('/', getStatus)
-
-router.post('/ping', validate(Joi.object({})), pingStatus)
+router.get('/ping', validate(Joi.object({})), pingStatus)
+router.get('/history', getPingHistory)
 
 export default router
