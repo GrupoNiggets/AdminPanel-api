6b11b119cdebb6e18b6c986f89ea18cfa4d22cc2
iRuizTorre <inigoruiztorreb@gmail.com>
2025-11-20 21:14:04 +0100

    mi booooombooo

--- src/modules/bugs/bugs.mongoose.js
+++ src/modules/bugs/bugs.mongoose.js
@@ -1,18 +1,35 @@
-import mongoose from 'mongoose'
-import { randomUUID } from 'crypto'
+import mongoose from "mongoose";
+import { randomUUID } from "crypto";
 
-const bugsSchema = new mongoose.Schema({
-  id: { type: String, default: () => randomUUID(), unique: true, index: true },
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  reporter: { type: String, required: true },
-  status: { type: String, enum: ['abierto', 'en progreso', 'resuelto'], default: 'abierto' },
-  priority: { type: String, enum: ['alta', 'media', 'baja'], default: 'media' }
-}, {
-  timestamps: { createdAt: 'createdAt', updatedAt: 'updatedAt' },
-  versionKey: false,
-  collection: 'bugs'
-})
+const bugsSchema = new mongoose.Schema(
+  {
+    id: {
+      type: String,
+      default: () => randomUUID(),
+      unique: true,
+      index: true,
+    },
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    reporter: { type: String, required: true },
+    status: {
+      type: String,
+      enum: ["ABIERTO", "EN PROGRESO", "RESUELTO"],
+      default: "ABIERTO",
+    },
+    priority: {
+      type: String,
+      enum: ["ALTA", "MEDIA", "BAJA"],
+      default: "MEDIA",
+    },
+  },
+  {
+    timestamps: { createdAt: "createdAt", updatedAt: "updatedAt" },
+    versionKey: false,
+    collection: "bugs",
+  }
+);
 
-export const BugsModel = mongoose.models.Bugs || mongoose.model('Bugs', bugsSchema)
-export default BugsModel
+export const BugsModel =
+  mongoose.models.Bugs || mongoose.model("Bugs", bugsSchema);
+export default BugsModel;
