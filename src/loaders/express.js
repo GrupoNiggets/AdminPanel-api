deb8bc30e2d6e437835cb0df89130b2f5dc5ed8d
Zer0Dev <76405774+Zer0Dev-exe@users.noreply.github.com>
2025-11-24 09:15:39 +0100

    Update SSL

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -13,7 +13,11 @@ export async function createExpressApp() {
   const app = express()
 
   app.set('trust proxy', 1)
-  app.use(
+  // Disable Helmet for Swagger docs to avoid CSP and COOP issues
+  app.use((req, res, next) => {
+    if (req.path.startsWith('/api/docs')) {
+      return next()
+    }
     helmet({
       contentSecurityPolicy: {
         directives: {
@@ -23,8 +27,9 @@ export async function createExpressApp() {
           imgSrc: ["'self'", 'data:', 'https:']
         }
       }
-    })
-  )
+    })(req, res, next)
+  })
+
   app.use(cors())
   app.use(express.json({ limit: '1mb' }))
   app.use(express.urlencoded({ extended: true }))
